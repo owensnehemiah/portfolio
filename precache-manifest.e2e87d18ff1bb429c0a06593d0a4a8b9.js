e840fd488aa94159653be8ecf4f8bb7bb47bc9d2
owensnehemiah <owens.nehemiah@gmail.com>
2020-05-09 13:13:36 -0400

    Updates

---
+++ precache-manifest.e2e87d18ff1bb429c0a06593d0a4a8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cdb5ca621088962121e30ceacb3594",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5f35c31fe123d8378b82",
+    "url": "/portfolio/static/css/main.afcf5445.chunk.css"
+  },
+  {
+    "revision": "8d93bf5d04cb71257ff4",
+    "url": "/portfolio/static/js/2.d992a2d3.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/portfolio/static/js/2.d992a2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f35c31fe123d8378b82",
+    "url": "/portfolio/static/js/main.782f7f6e.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "9a31018a64565b60059c5a747c3c82b4",
+    "url": "/portfolio/static/media/Scotland.9a31018a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c8335c1c8e9d090fc62c6d387ef90989.js"
+  "/portfolio/precache-manifest.e2e87d18ff1bb429c0a06593d0a4a8b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
