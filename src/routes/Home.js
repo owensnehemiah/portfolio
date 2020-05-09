12423c92d0d0a20903320b7feb0e884f4239775f
owensnehemiah <owens.nehemiah@gmail.com>
2020-05-09 11:47:42 -0400

    Updated Dependencies

---
+++ src/routes/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, Link, browserHistory, Industry } from 'react-router'
+
+class Home extends React.Component {
+    render() { 
+        return(
+            <div>
+                <h1>Home...</h1>
+            </div>
+        )
+    }
+
+}
+
+export { Home }
