c0b62b963c59728c24e6d77a95028690826fd2a7
owensnehemiah <owens.nehemiah@gmail.com>
2021-06-08 23:10:06 -0400

    Updates

---
+++ precache-manifest.9886f997f1f61c269e5f39473487402a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce0bb27871c8b65bffeb110a928c646",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "074a13d1fc58f4e694dc",
+    "url": "/portfolio/static/css/main.1b130803.chunk.css"
+  },
+  {
+    "revision": "8b48dbbe155cd3527fe4",
+    "url": "/portfolio/static/js/2.0ef7bf58.chunk.js"
+  },
+  {
+    "revision": "9c28a0d7395681e23bd21efb7aa27163",
+    "url": "/portfolio/static/js/2.0ef7bf58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074a13d1fc58f4e694dc",
+    "url": "/portfolio/static/js/main.b8443447.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "e1aeae459aebef1a290a548afa256438",
+    "url": "/portfolio/static/media/NeoOwensResume.e1aeae45.pdf"
+  },
+  {
+    "revision": "9a31018a64565b60059c5a747c3c82b4",
+    "url": "/portfolio/static/media/Scotland.9a31018a.jpg"
+  },
+  {
+    "revision": "008a921ef670223563c25d9210c99ea2",
+    "url": "/portfolio/static/media/portfolioPhoto2.008a921e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2462a1822cc08a2896cb9ff697f6c62d.js"
+  "/portfolio/precache-manifest.9886f997f1f61c269e5f39473487402a.js"
 );
 
 self.addEventListener('message', (event) => {
